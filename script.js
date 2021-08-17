1ca92397b2fb9df54da9731806f2ed1552601dcd
Sadak Halil <s.halil@yahoo.com>
2021-08-17 22:24:55 +0200

    16x16 grid with square divs, hover adds color

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const container = document.querySelector(".container");
 
+//create 16 x 16 grid
 for (let i = 1; i <= 16; i++) {
   for (let j = 1; j <= 16; j++) {
     const cell = document.createElement("div");
     cell.style.gridArea = `${i}/${j}/${i + 1}/${j + 1}`;
+    cell.classList.add("cell");
     container.appendChild(cell);
   }
 }
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+  cell.addEventListener("mouseover", (e) => {
+    e.target.style.background = "green";
+  });
+});
