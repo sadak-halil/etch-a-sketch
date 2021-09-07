a9771694276bf5cf01a3cb0608650fe998db1c87
Sadak Halil <sadak.halil@LEGO.com>
2021-09-07 16:50:21 +0300

    finalized

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
+'use strict'
 const canvas = document.querySelector(".canvas");
 //TODO: 
 // 1. add the drawing eventlistener in the create canvas function
 // 2. when reset is clicked, clean the container div and re-create the canvas 
 
 //initial canvas size
-let canvasSize = 16;
+let canvasSize = 100;
+
 //create canvas
-function createCanvas(camnvasSize){
+function createCanvas(canvasSize){
 for (let i = 1; i <= canvasSize; i++) {
   for (let j = 1; j <= canvasSize; j++) {
     const cell = document.createElement("div");
     cell.style.gridArea = `${i}/${j}/${i + 1}/${j + 1}`;
+    cell.style.width = `calc(90vh/${canvasSize})`;
+    cell.style.height = `calc(90vh  /${canvasSize})`;
     cell.classList.add("cell");
     canvas.appendChild(cell);
   }
-}};
-createCanvas(canvasSize);
-
-const cells = document.querySelectorAll(".cell");
+//declare a global variable from within the function scope with "window"
+window.cells = document.querySelectorAll(".cell");
+}
 cells.forEach((cell) => {
   cell.addEventListener("mouseover", (e) => {
     e.target.style.background = "black";
   });
 });
+};
+createCanvas(canvasSize);
+
+
 
 const resetButton = document.querySelector("button");
 resetButton.addEventListener("click", () =>{
-  size = parseInt(window.prompt("Size of the side of the new grid(# of squares)?)"));
-  cells.forEach((cell) =>{
-    cell.style.backgroundColor = "white";
-  });
+  let size = parseInt(window.prompt("Size of the side of the new grid(# of squares)?)"));
+  document.querySelector(".canvas").innerHTML = "";
+  createCanvas(size);
 });
